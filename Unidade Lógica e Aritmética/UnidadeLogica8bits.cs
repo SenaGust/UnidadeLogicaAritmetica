d0af37a5161162eeb91ef2d0e546f32c351cbe2e
Gustavo Sena <gustavosena98@LIVE.COM>
2018-10-30 12:00:08 -0300

    comentários

--- Unidade Lógica e Aritmética/UnidadeLogica8bits.cs
+++ Unidade Lógica e Aritmética/UnidadeLogica8bits.cs
@@ -8,5 +8,17 @@ namespace Unidade_Lógica_e_Aritmética
 {
     class UnidadeLogica8bits
     {
+        /*
+         * Tabela
+         * F2 F1 F0 Saída
+         * 0  0  0  A and B
+         * 0  0  1  A or B
+         * 0  1  0  Not A
+         * 0  1  1  Not B
+         * 1  0  0  A + B
+         * 1  0  1  A - B
+         * 1  1  0    -
+         * 1  1  1    -
+         */
     }
 }
