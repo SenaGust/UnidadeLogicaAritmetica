d9c7bfa9d452c236b34bc9d4d02d34a71acb9bcd
Gustavo Sena <gustavosena98@LIVE.COM>
2018-10-31 07:41:00 -0300

    Criação do "método imprimirBinário" e a implementação do método inteiroParaBinário.

--- Unidade Lógica e Aritmética/Conversor.cs
+++ Unidade Lógica e Aritmética/Conversor.cs
@@ -15,7 +15,26 @@ namespace Unidade_Lógica_e_Aritmética
          * Conversão binário para ponto flutuante
          * Conversão binário para hexadecimal
          */
-        public bool[] DecimalBinario()
+        public bool[] InteiroParaBinario(int tamanho, int numero)
+        {
+            bool[] resultado = new bool[tamanho];
+
+            for (int pos = tamanho - 1; pos >= 0; pos--)
+            {
+                if (numero % 2 == 0)
+                {
+                    resultado[pos] = false;
+                }
+                else //numero%2 == 1
+                {
+                    resultado[pos] = true;
+                }
+                numero /= 2;
+            }
+
+            return resultado;
+        }
+        public bool[] PontoFlutuanteParaBinario(float numero)
         {
             return null;
         }
@@ -31,9 +50,25 @@ namespace Unidade_Lógica_e_Aritmética
         {
             return 0;
         }
-        public string BinarioParaHexadecimal()
+        public string BinarioParaHexadecimal(string numero)
         {
             return null;
+        }
+
+        public string imprimirBinario(bool[] bin)
+        {
+            //não sei se isso pode ficar aqui, mas vai ser útil (Gustavo)
+            string result = null;
+
+            for (int pos = 0; pos < bin.Length; pos++)
+            {
+                if (bin[pos])
+                    result += "1";
+                else
+                    result += "0";
+            }
+
+            return result;  
         }
     }
 }
