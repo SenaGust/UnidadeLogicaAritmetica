7cc6deffde3486293a8b74b3eaaa50ab4e98ced0
Gustavo Sena <gustavosena98@LIVE.COM>
2018-11-04 15:45:13 -0200

    Implementação da Alu de 8 bits. Implementação dos botões and e or

--- Unidade Lógica e Aritmética/MainWindow.xaml.cs
+++ Unidade Lógica e Aritmética/MainWindow.xaml.cs
@@ -28,13 +28,49 @@ namespace Unidade_Lógica_e_Aritmética
         float x, y;
         bool val1, val2;
 
+        private void button_AandB_Click(object sender, RoutedEventArgs e)
+        {
+            bool[] f = { false, false, false };
+            chamarULA8Bits(Convert.ToInt32(textBoxOperando1.Text), Convert.ToInt32(textBoxOperando1.Text), f);
+        }
+
+        private void button_AorB_Click(object sender, RoutedEventArgs e)
+        {
+            bool[] f = { true, false, false };
+            chamarULA8Bits(Convert.ToInt32(textBoxOperando1.Text), Convert.ToInt32(textBoxOperando1.Text), f);
+        }
+
+        private void buttonSair_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
         private void buttonSoma_Click(object sender, RoutedEventArgs e)
         {
             x = Convert.ToSingle(textBoxOperando1.Text);
             y = Convert.ToSingle(textBoxOperando2.Text);
 
             //mudar para ponto flutuante
-            
+
+        }
+        private void chamarULA8Bits(int a, int b, bool[] f)
+        {
+            //objetos
+            Conversor Converter = new Conversor();
+            UnidadeLogica8bits alu = new UnidadeLogica8bits();
+
+            //converter os dois números inteiros para binários, nesse caso 8 bits máximo
+            bool[] numA = Converter.InteiroParaBinario(8, Convert.ToInt32(textBoxOperando1.Text));
+            bool[] numB = Converter.InteiroParaBinario(8, Convert.ToInt32(textBoxOperando2.Text));
+
+            //chamando ula
+            bool[] resultado = new bool[8];
+            alu.ULA8Bits(numA, numB, f, resultado);
+
+            //mostrando resultados
+            resultado10.Text = Converter.imprimirBinario(resultado);
+            textBoxOperando16A.Text = Converter.imprimirBinario(numA);
+            textBoxOperando16B.Text = Converter.imprimirBinario(numB);
         }
     }
 }
--- Unidade Lógica e Aritmética/UnidadeLogica8bits.cs
+++ Unidade Lógica e Aritmética/UnidadeLogica8bits.cs
@@ -8,6 +8,15 @@ namespace Unidade_Lógica_e_Aritmética
 {
     class UnidadeLogica8bits
     {
+        UnidadeLogica1bit ALU7 = new UnidadeLogica1bit();
+        UnidadeLogica1bit ALU6 = new UnidadeLogica1bit();
+        UnidadeLogica1bit ALU5 = new UnidadeLogica1bit();
+        UnidadeLogica1bit ALU4 = new UnidadeLogica1bit();
+        UnidadeLogica1bit ALU3 = new UnidadeLogica1bit();
+        UnidadeLogica1bit ALU2 = new UnidadeLogica1bit();
+        UnidadeLogica1bit ALU1 = new UnidadeLogica1bit();
+        UnidadeLogica1bit ALU0 = new UnidadeLogica1bit();
+
         /*
          * Tabela
          * F2 F1 F0 Saída
@@ -20,5 +29,26 @@ namespace Unidade_Lógica_e_Aritmética
          * 1  1  0    -
          * 1  1  1    -
          */
+        
+        public bool ULA8Bits(bool[] a, bool[] b, bool[] f, bool[] s)
+        {
+            bool vai1, vem1 = false;
+            s[7] = ALU7.ULA1Bit(a[7], b[7], f, vem1, out vai1);
+            vem1 = vai1;
+            s[6] = ALU7.ULA1Bit(a[6], b[6], f, vem1, out vai1);
+            vem1 = vai1;
+            s[5] = ALU7.ULA1Bit(a[5], b[5], f, vem1, out vai1);
+            vem1 = vai1;
+            s[4] = ALU7.ULA1Bit(a[4], b[4], f, vem1, out vai1);
+            vem1 = vai1;
+            s[3] = ALU7.ULA1Bit(a[3], b[3], f, vem1, out vai1);
+            vem1 = vai1;
+            s[2] = ALU7.ULA1Bit(a[2], b[2], f, vem1, out vai1);
+            vem1 = vai1;
+            s[1] = ALU7.ULA1Bit(a[1], b[1], f, vem1, out vai1);
+            vem1 = vai1;
+            s[0] = ALU7.ULA1Bit(a[0], b[0], f, vem1, out vai1);
+            return vai1;
+        }
     }
 }
